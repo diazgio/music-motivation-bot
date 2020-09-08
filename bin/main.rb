13b57e33811dded4a2cd0020481266b82a2bf19f
Giordano Dìaz <diazgiordano@gmail.com>
2020-09-08 09:43:21 -0500

    rspec file completed and main modify

--- bin/main.rb
+++ bin/main.rb
@@ -4,5 +4,6 @@ puts 'Hello, Welcome to Music motivation bot'
 puts '--------------------------------------'
 puts 'This bot has a colection of different music to all ocations'
 puts 'Just writte one of the follow categories and enjoid the music'
+sleep(3)
 
 Bot.new
