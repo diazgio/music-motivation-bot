33b011ab94d1103b480735f55ab4148ca781c935
Giordano Dìaz <diazgiordano@gmail.com>
2020-09-09 15:45:09 -0500

    Add changes to bot file, create initialize method to run test

---
+++ spec/bot_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/bot'
+describe Bot do
+  let(:bot) { Bot.new }
+
+  describe '#initialize' do
+    it 'Starts the Bot without error' do
+      expect(bot.class).to eql Bot
+    end
+  end
+end
