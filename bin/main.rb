dc3812c31697319be08a7b9950402112530e3b10
Giordano Dìaz <diazgiordano@gmail.com>
2020-09-09 10:24:10 -0500

    Add changes to main file, remove puts

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,3 @@
 require_relative '../lib/bot.rb'
 
-puts 'Hello, Welcome to Music motivation bot'
-puts '--------------------------------------'
-puts 'This bot has a colection of different music to all ocations'
-puts 'Just writte one of the follow categories and enjoid the music'
-sleep(3)
-
 Bot.new
